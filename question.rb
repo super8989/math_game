d6b605018264dbcb4b5518cc188e07d63e42d166
Sam <super8989@gmail.com>
2020-09-14 21:11:37 -0400

    Update Question class

--- question.rb
+++ question.rb
@@ -6,14 +6,10 @@ class Question
   end
 
   def new_question
-    a = rand(1..20)
-    b = rand(1..20)
+    a = rand(20)
+    b = rand(20)
     @question = "What does #{a} plus #{b} equal?"
     @answer = a + b
   end
 end
 
-question = Question.new
-
-puts question.question
-puts question.answer
